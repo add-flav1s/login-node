cadc81f15d72ae8f846a4c0c72f917b037081656
add-flav1s <aflavia17_@hotmail.com>
2021-12-10 22:26:53 -0300

    building a server and packages update.

--- index.js
+++ index.js
@@ -6,13 +6,19 @@ var path = require('path');
 const app = express();
 
 app.use(session({
-    secret: 'ssshhhhh'
+    secret: 'cookie_secret',
+    resave: true,
+    saveUninitialized: true
 }));
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
+
+app.post('/', (req, res) => {
+    res.render('index.html');
+})
 
 app.get('/', (req, res) => {
     res.render('index.html');
