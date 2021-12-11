5f30d2fe699af4dcfc3dd3007739f5135d8b48e7
add-flav1s <aflavia17_@hotmail.com>
2021-12-11 14:08:20 -0300

    creating the username and password request and logging into the form

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 
-
-
 const port = 8082;
 var path = require('path');
 const app = express();
+
+var login = 'admin';
+var password = 'admin';
 
 app.use(session({
     secret: 'cookie_secret',
@@ -23,12 +24,28 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 
 app.post('/', (req, res) => {
-    console.log(req.body.login);
-    res.render('index');
+
+    if (req.body.password == password && req.body.login == login) {
+        req.session.login = login;
+        res.render('logged', {
+            login: login
+        });
+    } else {
+
+        res.render('index');
+    }
 })
 
 app.get('/', (req, res) => {
-    res.render('index');
+
+    if (req.session.login) {
+        res.render('logged', {
+            login: login
+        })
+    } else {
+        res.render('index');
+    }
+
 })
 
 app.listen(port, () => {
