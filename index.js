4cb1a879f00260cfe839eadaf7444a64a7846ab3
add-flav1s <aflavia17_@hotmail.com>
2021-12-10 23:04:23 -0300

    configuring the body-parser and the form req

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const session = require('express-session');
+const bodyParser = require('body-parser');
+
+
 
 const port = 8082;
 var path = require('path');
@@ -10,6 +13,9 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -17,13 +23,16 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 
 app.post('/', (req, res) => {
-    res.render('index.html');
+    console.log(req.body.login);
+    res.render('index');
 })
 
 app.get('/', (req, res) => {
-    res.render('index.html');
+    res.render('index');
 })
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 })
+
+module.exports = app;
